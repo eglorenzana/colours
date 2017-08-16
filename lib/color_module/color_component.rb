5ffd4653d5aab76b5f8e265807ae723d4823ecc3
Erick G Lorenzana <erickg.lorenzana@gmail.com>
2017-08-16 15:33:58 -0500

    Style guide for color, color component and util, with RuboCop

--- lib/color_module/color_component.rb
+++ lib/color_module/color_component.rb
@@ -1,43 +1,44 @@
 module ColorModule
+  # Components for the color models
   class ColorComponent
-    alias :read_attribute_for_serialization :send
+    alias read_attribute_for_serialization send
     DELTA_VALUE = 0.01
     attr_reader :value, :name
     attr_reader :min_value
     attr_reader :max_value
+
     def initialize(name, *limit)
       @name = name.to_sym
       @min_value = limit.flatten.min
       @max_value = limit.flatten.max
       @value = @min_value
     end
+
     def active_model_serializer
-        ColorModule::Serializers::ColorComponentSerializer
-    end    
+      ColorModule::Serializers::ColorComponentSerializer
+    end
 
     def validate(value)
-      (@min_value..@max_value).include?(value)
+      (@min_value..@max_value).cover?(value)
     end
 
     def value=(value)
-      _value = value
-      validate(_value) ?  (@value =  _value) :  (raise ColorComponentError.new(object: @name, value: _value))
+      validate(value) ? @value = value : (raise ColorComponentError, object: @name, value: value)
     end
 
     def resume
       "#{@name}: #{@value}"
     end
-    
+
     def to_h
-      {@name => @value}
-    end
-    
-    def eql?(another_component)
-      first_part = [:class, :name].all? do |method_name|
-        self.send(method_name) == another_component.send(method_name)
-      end
-      first_part && ((value - another_component.value).abs <= DELTA_VALUE)
+      { @name => @value }
     end
 
+    def eql?(other)
+      first_part = %i[class name].all? do |method_name|
+        send(method_name) == other.send(method_name)
+      end
+      first_part && ((value - other.value).abs <= DELTA_VALUE)
+    end
   end
 end
